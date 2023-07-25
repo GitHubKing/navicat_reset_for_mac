06ee75b0d9c69f59826a32a253b1279944b28215
quyaoshun <quyaoshun@163.com>
2023-07-25 10:14:25 +0800

    Update reset_navicat.sh

--- reset_navicat.sh
+++ reset_navicat.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#/usr/libexec/PlistBuddy -c "print" ~/Library/Preferences/com.navicat.NavicatPremium.plist
-/usr/libexec/PlistBuddy -c "Delete :91F6C435D172C8163E0689D3DAD3F3E9" ~/Library/Preferences/com.navicat.NavicatPremium.plist
-/usr/libexec/PlistBuddy -c "Delete :B966DBD409B87EF577C9BBF3363E9614" ~/Library/Preferences/com.navicat.NavicatPremium.plist
 
-rm -rf ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/.*
+# @link: https://github.com/yhan219/navicat_reset_mac/blob/main/README.md
+
+/usr/libexec/PlistBuddy -c "Delete :91F6C435D172C8163E0689D3DAD3F3E9" ~/Library/Preferences/com.navicat.NavicatPremium.plist 2>/dev/null
+/usr/libexec/PlistBuddy -c "Delete :B966DBD409B87EF577C9BBF3363E9614" ~/Library/Preferences/com.navicat.NavicatPremium.plist 2>/dev/null
+
+rm -rf ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/.* 2>/dev/null
+
+echo "SUCCESS"
